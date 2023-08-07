b874303a42d395c9f454e4e0f7f0428349ea8218
Fatbonks <106663968+Fatbonks@users.noreply.github.com>
2023-08-07 23:02:39 +1200

    added leaderboard
    can display name for now

--- Global.gd
+++ Global.gd
@@ -1,9 +1,36 @@
 extends Node
 
 
+var metadata = {
+	"coins": 5,
+	"scores": 6
+	}
+var player_name = "bob"
 var number_ans = 500
 var number_ans_2 = 200
 var number_ans_3 = 100
 var amount_enemys_left = 0
 signal tower_pos(pos)
 signal display_current_ans(ans)
+
+func _ready():
+	
+	SilentWolf.configure({
+		"api_key": "0TCyTzg2Bi6NTDBg7Oxx99Ejb1jbAETj4Cwj9t9A",
+		"game_id": "Quizgame",
+		"log_level": 0
+		 })
+
+	SilentWolf.configure_scores({
+		"open_scene_on_close": "res://scenes/MainPage.tscn"
+	})
+	
+	SilentWolf.Scores.save_score("bob", get_score(3, 5), "main", metadata)
+	SilentWolf.Scores.save_score("rod", get_score(5, 1), "main", metadata)
+	SilentWolf.Scores.save_score("tod", get_score(2, 1), "main", metadata)
+	
+
+func get_score(score, coins):
+	
+	return score * 1000 + coins
+
---
+++ LeaderBoard.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var names = $names
+
+func _ready():
+	_update_leaderboard()
+
+func _update_leaderboard():
+	var sw_result = await SilentWolf.Scores.get_scores().sw_get_scores_complete
+	var scores = sw_result.scores
+	for score in scores:
+		names.add_item(score.player_name)
+		print("Player name: " + str(score.player_name) + ", score: " + str(score.score) + ", metadata: " + str(score.metadata))
