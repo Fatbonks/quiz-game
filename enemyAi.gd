32178ecf309a4b5f3dc2909e38bba14c592c1465
Fatbonks <106663968+Fatbonks@users.noreply.github.com>
2023-08-17 01:02:56 +1200

    more quiz questions added

--- enemyAi.gd
+++ enemyAi.gd
@@ -4,22 +4,28 @@ extends Node2D
 var lis = []
 var enemy_num: int
 func _ready():
-	var random_num = randi_range(1, 3)
-	if random_num == 1:
-		enemy_num = Global.number_ans
-	elif random_num == 2:
-		enemy_num = Global.number_ans_2
-	elif random_num == 3:
-		enemy_num = Global.number_ans_3
-	randomize()
-	for e in range(enemy_num+1):
-		var j = str(e, " + ", enemy_num-e)
-		lis.insert(0, j)
-	text.text = lis.pick_random()
-	
-	for number_x in range(enemy_num+1):
-		for number_y in range(enemy_num+1):
-			number_y
-		pass
-
-
+	#randomize()
+	#var random_num = randi_range(1, 3)
+	#if random_num == 1:
+	#	enemy_num = Global.number_ans
+	#elif random_num == 2:
+	#	enemy_num = Global.number_ans_2
+	#elif random_num == 3:
+	#	enemy_num = Global.number_ans_3
+	#for e in range(enemy_num+1):
+	#	var j = str(e, " + ", enemy_num-e)
+	#	lis.insert(0, j)
+	#text.text = lis.pick_random()
+	#print(lis)
+	#for number_x in range(enemy_num+1):
+	#	for number_y in range(enemy_num+1):
+	#		var mult_num = number_y * number_x
+	#		if mult_num == enemy_num:
+	#			var temp_num = str(number_x, " * ", number_y)
+	#			lis.insert(0, temp_num)
+	#text.text = lis.pick_random()
+	for i in range(250):
+		var test = 250 % (i+1)
+		if test == 0:
+			var test_num = 250 / (i+1)
+			print(test_num,  "*", (i+1), "= 250")
